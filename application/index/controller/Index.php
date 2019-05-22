347a8e8c7bd4c5968dd9c164a8dbc357cac0a7cd
Billy丶斌 <641833868@qq.com>
2019-05-22 11:35:54 +0800

    122

---
+++ application/index/controller/Index.php
@@ -0,0 +1,23 @@
+<?php
+namespace app\index\controller;
+
+
+use think\Controller;
+use think\Request;
+
+class Index extends Controller
+{
+    
+    // 获取用户信息
+    public function read($id = 0)
+    {
+        $user = ['aaaa','111'];
+        if ($user) {
+            return json($user);
+        } else {
+            // 抛出HTTP异常 并发送404状态码   
+            abort(404,'用户不存在');
+        }
+    }
+
+}
