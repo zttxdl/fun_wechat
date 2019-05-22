70ff4ec0c4506505ca074c4fad4375c2d743ce39
Billy丶斌 <641833868@qq.com>
2019-05-22 11:31:22 +0800

    定义好，接口返回类型

--- application/common.php
+++ application/common.php
@@ -10,3 +10,45 @@
 // +----------------------------------------------------------------------
 
 // 应用公共文件
+
+ /**
+ * 返回封装后的API成功方法
+ * @access protected
+ * @param  mixed     $data 要返回的数据
+ * @param  integer   $code 返回的code
+ * @param  mixed     $msg 提示信息
+ * @return void
+ */
+if (!function_exists('json_success')) {
+    function json_success($data, $msg = '',$code = 200)
+    {
+        $result = [
+            'code' => $code,
+            'data' => $data,
+            'msg'  => $msg,
+            'time' => $_SERVER['REQUEST_TIME'],
+        ];
+
+        return json($result);
+    }
+}
+
+ /**
+ * 返回封装后的API失败方法
+ * @access protected
+ * @param  integer   $code 返回的code
+ * @param  mixed     $msg 提示信息
+ * @return void
+ */
+if (!function_exists('json_error')) {
+    function json_error($msg = '',$code = 201)
+    {
+        $result = [
+            'code' => $code,
+            'msg'  => $msg,
+            'time' => $_SERVER['REQUEST_TIME'],
+        ];
+
+        return json($result);
+    }
+}
