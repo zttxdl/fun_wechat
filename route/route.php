3661730fff0613842ded5dbc3c6846874efbed30
Billy丶斌 <641833868@qq.com>
2019-05-23 15:44:58 +0800

    定义路由

--- route/route.php
+++ route/route.php
@@ -9,5 +9,11 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 
-
-Route::get('admin/index','admin/Admin/index');
+//登录注册用户组
+Route::group('merchants',function (){
+        Route::rule('login','merchants/Login/login');
+        Route::rule('register','merchants/Login/register');
+        Route::rule('updatePasswor','merchants/Login/updatePasswor');
+        Route::rule('phoneValidate','merchants/Login/phoneValidate');
+        Route::rule('getMobileCode','merchants/Login/getMobileCode');
+});
