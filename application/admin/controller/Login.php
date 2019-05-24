3f9ba61c4aebe131e3b169ec36e645d400b21939
zhangtaotao <835303992@qq.com>
2019-05-24 17:04:54 +0800

    后台用户登录

--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -3,33 +3,7 @@
 
 namespace app\admin\controller;
 
-<<<<<<< HEAD
-use app\common\Auth\JwtAuth;
-use think\Request;
 
-class Login
-{
-    public function index()
-    {
-
-    }
-
-    public function login(Request $request)
-    {
-        $username = $request->param('username');
-        $password = $request->param('password');
-
-        // 去数据库或者缓存中验证该用户 获取用户信息的 uid
-
-        $uid = 1;
-
-        $jwtAuth = JwtAuth::getInstance();
-        $token = $jwtAuth->setUid($uid)->encode()->getToken();
-
-        return json_success('200',[
-            'token' => $token
-        ]);
-=======
 use think\App;
 use think\Controller;
 use think\facade\Validate;
@@ -90,6 +64,5 @@ class Login extends Controller
     {
         $captcha = new Captcha();
         return $captcha->entry();
->>>>>>> ztt
     }
 }
