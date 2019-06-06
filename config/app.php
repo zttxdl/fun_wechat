d94e582da24e2de28e033b4cdcd1e4059f4a5886
Billy丶斌 <641833868@qq.com>
2019-06-06 13:59:21 +0800

    微信支付配置证书

--- config/app.php
+++ config/app.php
@@ -204,5 +204,16 @@ return [
             'file' => __DIR__.'/wechat_rider.log',
         ],
     ],
+    'wx_pay'=>[
+        // 必要配置
+        'app_id'             => 'wx7e84dbf300d4764d',
+        'mch_id'             => '1538416851',
+        'key'                => 'iew0a4ek8d2ap5nvn78bnsoq7m3wlfcs',   // API 密钥
+        // 如需使用敏感接口（如退款、发送红包等）需要配置 API 证书路径(登录商户平台下载 API 证书)
+        'cert_path'          => './../extend/wechat/key_cert/apiclient_cert.pem', // XXX: 绝对路径！！！！
+        'key_path'           => './../extend/wechat/key_cert/apiclient_key.pem',      // XXX: 绝对路径！！！！
+        'notify_url'         => '',     // 你也可以在下单时单独设置来想覆盖它
+        'sandbox' => false,
+    ]
 
 ];
