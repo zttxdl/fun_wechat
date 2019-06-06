276d12007f992ef15bba1a7159335c6c772a3eef
Lxk <l_xiaokun@163.com>
2019-06-06 18:00:50 +0800

    授权快捷登录

--- extend/wx_auth_phone/wxBizDataCrypt.php
+++ extend/wx_auth_phone/wxBizDataCrypt.php
@@ -1,4 +1,6 @@
 <?php
+include_once "errorCode.php";
+
 
 /**
  * 对微信小程序用户加密数据的解密示例代码.
@@ -10,11 +12,6 @@ class WXBizDataCrypt
     private $appid;
 	private $sessionKey;
 
-	public static $OK = 0;
-	public static $IllegalAesKey = -41001;
-	public static $IllegalIv = -41002;
-	public static $IllegalBuffer = -41003;
-	public static $DecodeBase64Error = -41004;
 	/**
 	 * 构造函数
 	 * @param $sessionKey string 用户在小程序登录后获取的会话密钥
@@ -39,13 +36,13 @@ class WXBizDataCrypt
 	public function decryptData( $encryptedData, $iv, &$data )
 	{
 		if (strlen($this->sessionKey) != 24) {
-			return $this->IllegalAesKey;
+			return ErrorCode::$IllegalAesKey;
 		}
 		$aesKey=base64_decode($this->sessionKey);
 
         
 		if (strlen($iv) != 24) {
-			return $this->IllegalIv;
+			return ErrorCode::$IllegalIv;
 		}
 		$aesIV=base64_decode($iv);
 
@@ -56,14 +53,14 @@ class WXBizDataCrypt
 		$dataObj=json_decode( $result );
 		if( $dataObj  == NULL )
 		{
-			return $this->IllegalBuffer;
+			return ErrorCode::$IllegalBuffer;
 		}
 		if( $dataObj->watermark->appid != $this->appid )
 		{
-			return $this->IllegalBuffer;
+			return ErrorCode::$IllegalBuffer;
 		}
 		$data = $result;
-		return $this->OK;
+		return ErrorCode::$OK;
 	}
 
 }
