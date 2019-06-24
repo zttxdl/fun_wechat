b195e7e5748345cd8a9f6d00b5e54a41c35af02f
ztt <835303992@qq.com>
2019-06-24 11:51:30 +0800

    1

--- route/route.php
+++ route/route.php
@@ -11,7 +11,7 @@
 
 //解决跨域问题
 header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers:Origin, X-Requested-With, Content-Type, Accept");
+header("Access-Control-Allow-Headers:Origin, X-Requested-With, Content-Type, Accept, api-token");
 header('Access-Control-Allow-Methods: POST,GET,PUT');
 if(request()->isOptions()){
     exit();
