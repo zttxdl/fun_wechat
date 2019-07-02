83de00e6753e0743c484e180b4b21c881e279333
Billy丶斌 <641833868@qq.com>
2019-07-02 15:44:50 +0800

    图片上传

--- application/merchants/controller/Upload.php
+++ application/merchants/controller/Upload.php
@@ -20,31 +20,31 @@ class Upload extends MerchantsBase
      * @return \think\Response
      */
     // 文件上传提交
+    // public function up(Request $request)
+    // {
+    //     // 获取表单上传文件
+    //     $file = $request->file('file');
+
+    //     error_log(print_r($file,1),3,Env::get('root_path')."./logs/file.log");
+    //     // 上传文件验证
+    //     $result = $this->validate(['file' => $file], ['file'=>'require|image'],['file.require' => '请选择上传文件', 'file.image' => '非法图像文件']);
+    //     if(true !== $result){
+    //         $this->error($result);
+    //     }
+    //     // 移动到框架应用根目录 目录下
+    //     $info = $file->move('./uploads/merchant/'.$this->shop_id);
+    //     if ($info) {
+
+    //         $data['images'] = '/uploads/merchant/'.$this->shop_id.'/'.$info->getSaveName();
+    //         $this->success('文件上传成功',$data);
+    //     } else {
+    //         // 上传失败获取错误信息
+    //         $this->error($file->getError());
+    //     }
+
+    // }
+
     public function up(Request $request)
-    {
-        // 获取表单上传文件
-        $file = $request->file('file');
-
-        error_log(print_r($file,1),3,Env::get('root_path')."./logs/file.log");
-        // 上传文件验证
-        $result = $this->validate(['file' => $file], ['file'=>'require|image'],['file.require' => '请选择上传文件', 'file.image' => '非法图像文件']);
-        if(true !== $result){
-            $this->error($result);
-        }
-        // 移动到框架应用根目录 目录下
-        $info = $file->move('./uploads/merchant/'.$this->shop_id);
-        if ($info) {
-
-            $data['images'] = '/uploads/merchant/'.$this->shop_id.'/'.$info->getSaveName();
-            $this->success('文件上传成功',$data);
-        } else {
-            // 上传失败获取错误信息
-            $this->error($file->getError());
-        }
-
-    }
-
-    public function upload(Request $request)
     {
         // 获取表单上传文件
         $file = $request->file('file');
