74a3dbc670ab0ed47bbc342a974fcdb2890120be
Billy丶斌 <641833868@qq.com>
2019-07-11 17:08:18 +0800

    超时订单脚本

--- application/api/controller/AutoShell.php
+++ application/api/controller/AutoShell.php
@@ -55,7 +55,38 @@ class AutoShell extends Controller
 
     }
 
+    /**
+     * 超时订单自动取消 付款减库存需要加上库存（每分钟/次）
+     */
+    public function cancelOrders()
+    {
+        $orderlist=Db::table('fun_orders')->where('add_time','<',time()-15*60)->where('pay_status',0)->where('status',1)->select();
 
+        foreach ($orderlist as $k => $v) {
+            Db::table('fun_orders')->where('id',$v['id'])->update(['trading_closed_time'=>time(),'status'=>9]);
+            //付款减库存的商品
+            $goodslist=Db::table('fun_orders_info')->where('orders_id',$v['id'])->field('product_id,num')->select();
+
+            //如果使用红包 状态回滚
+            if($v['platform_coupon_money'] > 0){
+                Db::table('fun_my_coupon')->where('id',$v['platform_coupon_id'])->setField('status',1);
+
+            }
+
+            foreach ($goodslist as $key => $value) {
+                $today = date('Y-m-d',time());
+                //加库存
+                Db::table('fun_today_deals')
+                    ->where('product_id',$value['product_id'])
+                    ->where('today',$today)
+                    ->setInc('num',$value['num']);
+            }
+        }
+
+        $num = count($orderlist);
+
+        echo $num;
+    }
 
 
     
