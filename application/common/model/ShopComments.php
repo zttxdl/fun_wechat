86cdc1568d8eda9e4216ce44dc11043d667557a3
Lxk <l_xiaokun@163.com>
2019-07-16 17:34:23 +0800

    订单实时推送测试

--- application/common/model/ShopComments.php
+++ application/common/model/ShopComments.php
@@ -12,7 +12,7 @@ class ShopComments extends Model
         $where[] = ['shop_id','=',$shop_id];
         $count = $this->where($where)->count();
         $sum = $this->where($where)->sum('star');
-
+        // 此处评分计算， 可将控制语句删除，因流程问题，如果走到这个方法的话， $count 是不可能存在为 0 的情况的
         if ($count != 0){
             $star = round($sum / $count,2);
         }else{
