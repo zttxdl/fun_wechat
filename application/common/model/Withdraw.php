7875235a415e98d7b1a161b2876196d9f44a7551
ztt <835303992@qq.com>
2019-07-23 21:05:26 +0800

    退款相关bug

--- application/common/model/Withdraw.php
+++ application/common/model/Withdraw.php
@@ -18,16 +18,15 @@ class Withdraw extends Model
      */
     public function getAcountMoney($shop_id)
     {
-        //已结算收入
-        $shouru_money = $this->where([['shop_id','=',$shop_id],['status','=',1],['type','=',1],['add_time','<',strtotime(date('Y-m-d')) ]])
-            ->sum('money');
+        //商家总收入 排除 1 4 9 10 11的订单
+        $shouru_money = model('Shop')->getCountSales($shop_id);
 
         //提现过程中的金额【包括 `已提现`，`申请提现`】
         $tx_money = $this->where([['shop_id','=',$shop_id],['type','=',2],['status','in','1,3']])->sum('money');
 
-        $acount_money = $shouru_money + $tx_money;
+        $acount_money = $shouru_money - $tx_money;
 
-        return $acount_money;
+        return sprintf("%.2f",$acount_money);
     }
 
 }
