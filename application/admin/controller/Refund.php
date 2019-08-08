7a797de1b42590e4a525b00ebcedc7fb84023e55
zhangtaotao <835303992@qq.com>
2019-08-08 16:26:03 +0800

    '管理后台退款模块'

--- application/admin/controller/Refund.php
+++ application/admin/controller/Refund.php
@@ -10,6 +10,11 @@ use think\Db;
 
 class Refund extends Base
 {
+    private $status = [
+        '1' => '申请退款',
+        '2' => '退款成功',
+        '3' => '退款失败',
+    ];
     /**
      * 退单列表
      * @param Request $request
@@ -24,11 +29,11 @@ class Refund extends Base
 
         $map = [];
         if($search) {
-            $map[] = ['a.out_trade_no|a.out_refund_no|b.shop_name|c.nikename','like','%'.$search.'%'];
+            $map[] = ['a.out_trade_no|a.out_refund_no|b.shop_name|c.nickname','like','%'.$search.'%'];
         }
 
         if($status) {
-            $map[] = ['status','=',$status];
+            $map[] = ['a.status','=',$status];
         }
 
         $refundList = Db::name('refund')
@@ -41,7 +46,14 @@ class Refund extends Base
                         ->paginate($page_size)
                         ->toArray();
 
-        dump($refundList);
+        foreach ($refundList['data'] as &$value)
+        {
+            $value['add_time'] = date('Y-m-d H:i:s',$value['add_time']);
+            $value['status'] = $this->status[$value['status']];
+        }
+
+
+        $this->success('获取成功',$refundList);
 
     }
 
@@ -50,25 +62,44 @@ class Refund extends Base
      */
     public function getDetail(Request $request)
     {
-        $order_id = $request->param('id');
+        $refund_id = $request->param('id');
 
-        if(empty($order_id)) {
+        if(empty($refund_id)) {
             $this->error('非法传参');
         }
 
         $result = [];
         $list = Db::name('refund')
-                ->field('out_refund_no,num,refund_fee,status,add_time,content')
-                ->where('order_id',$order_id)
+                ->field('out_refund_no,num,refund_fee,status,add_time,content,orders_id')
+                ->where('id',$refund_id)
                 ->find();
+        if(!$list) {
+            $this->error('暂无订单明细');
+        }
+        $list['add_time'] = date('Y-m-d H:i:s',$list['add_time']);
+        $result['base_info'] = $list;
 
         $details = Db::name('orders_info')->alias('a')
                     ->leftJoin('product b','a.product_id = b.id')
                     ->field('a.product_id,b.name,a.num,a.attr_ids,b.products_classify_id,b.price')
-                    ->where('orders_id','=',$order_id)
+                    ->where('a.id','=',$list['orders_id'])
                     ->select();
+        foreach ($details as &$val)
+        {
+            $val['attr_ids'] = model('ProductAttrClassify')->getNameByIds($val['attr_ids']);
+            $val['products_classify_id'] = model('ProductsClassify')->getNameById($val['products_classify_id']);
+        }
+        $result['goods_info'] = $details;
 
+        $orders = model('Orders')->getOrderById($list['orders_id']);
 
+        $result['order_info'] = [
+            'orders_sn' => $orders['orders_sn'],
+            'add_time' => date('Y-m-d H:i:s',$orders['add_time']),
+            'pay_time' => date('Y-m-d H:i:s',$orders['pay_time']),
+        ];
+
+        $this->success('success',$result);
 
     }
 
