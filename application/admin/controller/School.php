2d408d25ff118995c1f31a0463bf3f74822e9fa1
Lxk <l_xiaokun@163.com>
2019-09-06 16:06:10 +0800

    展示编辑学校追加字段

--- application/admin/controller/School.php
+++ application/admin/controller/School.php
@@ -107,7 +107,7 @@ class School extends Base
      */
     public function edit($id)
     {
-        $info = Db::name('school')->where('id','=',$id)->field('id,name,fid,latitude,longitude')->find();
+        $info = Db::name('school')->where('id','=',$id)->field('id,name,fid,latitude,longitude,completion_time,fetch_time')->find();
         $info['cname'] = Db::name('school')->where('id','=',$info['fid'])->value('name');
         $area_list = Db::name('school')->where('level','=',1)->field('id,name')->select();
         $canteen_list = Db::name('canteen')->where('school_id','=',$id)->select();
