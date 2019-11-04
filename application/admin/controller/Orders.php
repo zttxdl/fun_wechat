067079c1163286154b999c94ca18db2c70152f6d
Lxk <l_xiaokun@163.com>
2019-11-04 16:03:32 +0800

    运营平台的订单详情处理

--- application/admin/controller/Orders.php
+++ application/admin/controller/Orders.php
@@ -156,8 +156,6 @@ class Orders extends Base
 
         // 获取商家实际收入
         $shop_income_money = model('Withdraw')->getMoneyByOrderSn($list['orders_sn']);//商家实际收入 = 商家收支明细表money字段
-        //获取平台收入
-        $platform_choucheng = sprintf('%.2f',$list['money'] - $shop_income_money - $list['ping_fee'] - $list['shitang_choucheng']);
 
         //订单信息 
         $result['order_info'] = [
@@ -176,7 +174,7 @@ class Orders extends Base
             'num' => $list['num'],
             'box_money' => $list['box_money'],
             'remark' => $list['message'],
-            'platform_choucheng' => $platform_choucheng,
+            'platform_choucheng' => $list['platform_choucheng'],
             'shitang_choucheng' => $list['shitang_choucheng'],
             'hongbao_choucheng' => $list['hongbao_choucheng'],
             'shop_income_money' => $shop_income_money,
